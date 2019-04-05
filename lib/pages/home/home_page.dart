44b9b487308703391aa93b43032310a2a072b58c
Scott Cornell <scornell@trilixtech.com>
2019-04-05 18:12:36 -0400

    module6 complete

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -7,6 +7,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         leading: Icon(
           Icons.menu,
+          size: 40,
+          color: Colors.yellow,
         ),
         title: const Text('Home'),
         actions: <Widget>[
@@ -21,9 +23,18 @@ class HomePage extends StatelessWidget {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          const Text('one'),
+          const Text('one',
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline)),
           const Text('two'),
           const Text('three'),
+          Image.network(
+            'https://images.manning.com/720/960/resize/video/c/79a726d-5a58-4be4-9162-99bd63467b98/livevideo-flutter-in-motion-meap.png',
+            height: 200,
+          ),
         ],
       )),
     );
