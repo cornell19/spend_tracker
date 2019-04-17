903a1ede000549fdad446eae5b46cc48ecb037e5
Scott Cornell <scornell@trilixtech.com>
2019-04-16 20:10:14 -0400

    unit4 module 3 final

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:spend_tracker/pages/home/widgets/custom_text.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -24,38 +23,93 @@ class HomePage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          _TotalBudget(amount: amount),
           Container(
-            height: 100,
-            margin: EdgeInsets.all(10),
-            alignment: Alignment.center,
-            child: Text(
-              '\$$amount',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.green,
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.green,
-                  Colors.white54,
-                  Colors.blueGrey,
+              padding: EdgeInsets.only(bottom: 10),
+              height: MediaQuery.of(context).size.height - 196,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _BarLine(100, Colors.red, 'Withdraw', 506),
+                  _BarLine(400, Colors.green, 'Deposit', 1709)
                 ],
-                stops: [0.85, 0.95, 1.0],
-              ),
-              boxShadow: [
-                BoxShadow(color: Colors.grey, offset: Offset(4, 4)),
-              ],
-              borderRadius: BorderRadius.all(
-                Radius.circular(5),
-              ),
-            ),
-          ),
+              )),
         ],
+      ),
+    );
+  }
+}
+
+class _BarLine extends StatelessWidget {
+  const _BarLine(
+    this.height,
+    this.color,
+    this.label,
+    this.amount, {
+    Key key,
+  }) : super(key: key);
+  final double height;
+  final String label;
+  final Color color;
+  final int amount;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: height,
+          width: 100,
+          color: color,
+        ),
+        Text(label),
+        Text('\$$amount'),
+      ],
+    );
+  }
+}
+
+class _TotalBudget extends StatelessWidget {
+  const _TotalBudget({
+    Key key,
+    @required this.amount,
+  }) : super(key: key);
+
+  final String amount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      margin: EdgeInsets.all(10),
+      alignment: Alignment.center,
+      child: Text(
+        '\$$amount',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 40,
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.green,
+            Colors.white54,
+            Colors.blueGrey,
+          ],
+          stops: [0.85, 0.95, 1.0],
+        ),
+        boxShadow: [
+          BoxShadow(color: Colors.grey, offset: Offset(4, 4)),
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
+        ),
       ),
     );
   }
