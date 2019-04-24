f05793b274270aa241371c27625e5e5eae002b39
Scott Cornell <scornell@trilixtech.com>
2019-04-24 18:29:27 -0400

    Unit 4 module 6

--- lib/pages/home/widgets/menu.dart
+++ lib/pages/home/widgets/menu.dart
@@ -3,22 +3,102 @@ import 'package:flutter/material.dart';
 class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: const Text('Accounts'),
-            onTap: () => Navigator.of(context).pushNamed('/accounts'),
+    var color = Theme.of(context).primaryColor;
+    return SizedBox(
+      width: 150,
+      child: Drawer(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 100,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'MENU',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: color,
+                ),
+              ),
+            ),
+            Divider(
+              height: 20,
+              color: Colors.black,
+            ),
+            _MenuItem(
+                title: 'Accounts',
+                color: color,
+                icon: Icons.account_balance,
+                onTap: () => onNavigate(context, '/accounts')),
+            Divider(
+              height: 20,
+              color: Colors.black,
+            ),
+            _MenuItem(
+                title: 'Budget Items',
+                color: color,
+                icon: Icons.attach_money,
+                onTap: () => onNavigate(context, '/items')),
+            Divider(
+              height: 20,
+              color: Colors.black,
+            ),
+            _MenuItem(
+                title: 'Types',
+                color: color,
+                icon: Icons.widgets,
+                onTap: () => onNavigate(context, '/types')),
+            Divider(
+              height: 20,
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onNavigate(BuildContext context, String uri) {
+    Navigator.of(context).pop();
+    Navigator.of(context).pushNamed(uri);
+  }
+}
+
+class _MenuItem extends StatelessWidget {
+  const _MenuItem({
+    Key key,
+    @required this.color,
+    @required this.title,
+    @required this.icon,
+    @required this.onTap,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String title;
+  final Color color;
+  final Function onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Opacity(
+        opacity: 0.6,
+        child: Container(
+          height: 70,
+          alignment: Alignment.center,
+          child: Column(
+            children: <Widget>[
+              Icon(icon, color: color, size: 50.0),
+              Text(
+                title,
+                style: TextStyle(
+                  color: color,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              )
+            ],
           ),
-          ListTile(
-            title: const Text('Budget Items'),
-            onTap: () => Navigator.of(context).pushNamed('/items'),
-          ),
-          ListTile(
-            title: const Text('Types'),
-            onTap: () => Navigator.of(context).pushNamed('/types'),
-          ),
-        ],
+        ),
       ),
     );
   }
