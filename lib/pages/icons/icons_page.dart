fc6bcfa213a3effd7b9f6a488287c139e3de70da
Scott Cornell <scornell@trilixtech.com>
2019-05-08 20:25:39 -0400

    unit 5 module 2

--- lib/pages/icons/icons_page.dart
+++ lib/pages/icons/icons_page.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import './icon_list.dart';
 
 class IconsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var color = Theme.of(context).primaryColor;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Icons'),
       ),
-      body: Center(
-        child: const Text('Icons'),
+      body: SingleChildScrollView(
+        child: Wrap(
+          spacing: 10,
+          runSpacing: 10,
+          children: icons
+              .map(
+                (iconData) => InkWell(
+                      child: Opacity(
+                        opacity: .7,
+                        child: Icon(
+                          iconData,
+                          size: 60,
+                          color: color,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).pop<IconData>(iconData);
+                      },
+                    ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
