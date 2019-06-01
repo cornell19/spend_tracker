0a396c00c6e6c3e594a218b02f992c8e98225c3d
Scott Cornell <scornell@trilixtech.com>
2019-06-01 13:49:58 -0400

    Unit6 module 1 final

--- lib/application.dart
+++ lib/application.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:spend_tracker/routes.dart';
+
+import 'database/db_provider.dart';
 
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Spend Tracker',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        initialRoute: '/',
-        routes: routes);
+    return Provider<DbProvider>(
+      builder: (_) => DbProvider(),
+      dispose: (_, value) => value.dispose(),
+      child: MaterialApp(
+          title: 'Spend Tracker',
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+          ),
+          initialRoute: '/',
+          routes: routes),
+    );
   }
 }
