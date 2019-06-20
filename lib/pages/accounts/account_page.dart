7fe3dfaaffbcdde40a4fc2cd17683744537ee056
Scott Cornell <scornell@trilixtech.com>
2019-06-20 17:30:26 -0400

    unit 8 module3 final

--- lib/pages/accounts/account_page.dart
+++ lib/pages/accounts/account_page.dart
@@ -12,10 +12,14 @@ class AccountPage extends StatefulWidget {
   _AccountPageState createState() => _AccountPageState();
 }
 
-class _AccountPageState extends State<AccountPage> {
+class _AccountPageState extends State<AccountPage>
+    with TickerProviderStateMixin {
   Map<String, dynamic> _data;
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool _hasChanges = false;
+  AnimationController _controller;
+  Animation<Offset> _animationName;
+  Animation<Offset> _animationBalance;
 
   @override
   void initState() {
@@ -26,6 +30,25 @@ class _AccountPageState extends State<AccountPage> {
       _data = Map<String, dynamic>();
       _data['codePoint'] = Icons.add.codePoint;
     }
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _animationName = Tween<Offset>(begin: Offset(-3, 0), end: Offset(0, 0))
+        .animate(CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.50, 1.0, curve: Curves.easeInOutBack)));
+    _animationBalance = Tween<Offset>(begin: Offset(-3, 0), end: Offset(0, 0))
+        .animate(CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.0, 0.5, curve: Curves.easeInOutBack)));
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
   }
 
   @override
@@ -96,27 +119,35 @@ class _AccountPageState extends State<AccountPage> {
                     });
                   },
                 ),
-                TextFormField(
-                  initialValue:
-                      widget.account != null ? widget.account.name : '',
-                  decoration: InputDecoration(labelText: 'Name'),
-                  validator: (String value) {
-                    if (value.isEmpty) return 'Required';
-                  },
-                  onSaved: (String value) => _data['name'] = value,
+                SlideTransition(
+                  position: _animationName,
+                  child: TextFormField(
+                    initialValue:
+                        widget.account != null ? widget.account.name : '',
+                    decoration: InputDecoration(labelText: 'Name'),
+                    validator: (String value) {
+                      if (value.isEmpty) return 'Required';
+                    },
+                    onSaved: (String value) => _data['name'] = value,
+                  ),
                 ),
-                TextFormField(
-                  initialValue: widget.account != null
-                      ? widget.account.balance.toString()
-                      : '',
-                  decoration: InputDecoration(labelText: 'Balance'),
-                  keyboardType: TextInputType.numberWithOptions(decimal: true),
-                  validator: (String value) {
-                    if (value.isEmpty) return 'Required';
-                    if (double.tryParse(value) == null) return 'Invalid number';
-                  },
-                  onSaved: (String value) =>
-                      _data['balance'] = double.parse(value),
+                SlideTransition(
+                  position: _animationBalance,
+                  child: TextFormField(
+                    initialValue: widget.account != null
+                        ? widget.account.balance.toString()
+                        : '',
+                    decoration: InputDecoration(labelText: 'Balance'),
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    validator: (String value) {
+                      if (value.isEmpty) return 'Required';
+                      if (double.tryParse(value) == null)
+                        return 'Invalid number';
+                    },
+                    onSaved: (String value) =>
+                        _data['balance'] = double.parse(value),
+                  ),
                 )
               ],
             ),
